092bf7ef3932c10076271716aa1586df37df313c
jhonatan <jhonatandev@outlook.com>
2025-04-17 15:00:42 -0300

    chore: initial spring boot setup

---
+++ src/main/java/myrealproject/tracklift/TrackLiftApplication.java
@@ -0,0 +1,13 @@
+package myrealproject.tracklift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackLiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackLiftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/myrealproject/tracklift/TrackLiftApplicationTests.java
@@ -0,0 +1,13 @@
+package myrealproject.tracklift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackLiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
